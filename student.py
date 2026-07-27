9df3288dce16293b0257662075bf1397c657722b
ishan <ishan@MacBookAir.local>
2026-07-28 08:27:02 +1000

    student changed print

--- student.py
+++ student.py
@@ -2,6 +2,6 @@ def show_student():
     name = "Ishaan"
     course = "Masters in Data Science"
 
-    print("Student Information")
+    print("Student Information this is where i am making a change in main branch")
     print(f"Name: {name}")
     print(f"Course: {course}")
